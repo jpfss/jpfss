c5c4474208619c41d9b0c78be86f02779c47a459
jpfss <jingpengfeiss@outlook.com>
2023-07-18 14:52:42 +0800

    补充修改

--- src/basis/Constants.cpp
+++ src/basis/Constants.cpp
@@ -24,10 +24,27 @@ Constants &Constants::operator=(const Constants &rhs) {
 void Constants::go() {
 
   // Integer Literals
-
+  // 212        // Legal
+  // 215u   // Legal
+  // 0xFeeL // Legal
+  // 078    // Illegal: 8 is not an octal digit
+  // 032UU  // Illegal: cannot repeat a suffix
+  // 85         // decimal
+  // 0213       // octal
+  // 0x4b       // hexadecimal
+  // 30         // int
+  // 30u        // unsigned int
+  // 30l        // long
+  // 30ul       // unsigned long
   // Floating-point Literals
 
-  // Boolean Literals
+  // 3.14159       // Legal
+  // 314159E-5L    // Legal
+  // 510E          // Illegal: incomplete exponent
+  // 210f          // Illegal: no decimal or exponent
+  //.e55          // Illegal: missing integer or fraction
+
+  //  Boolean Literals
 
   // Character Literals
 
